01fbfd9b081e183a1317abc3da9c26e757d31d5e
ahihi <bdsthocu20@gmail.com>
2024-01-02 15:54:46 +0700

    #9

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,12 +1,89 @@
+import React from "react";
+import { useState } from "react";
+import "./Product.scss";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
 
+
+const images = [
+    "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto-compress&cs=tinysrgb&w=1600&lazy=load",
+    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+]
 
 const Product = () => {
 
+    const [selectedImg, setSelectedImg] = useState(0);
+    const [quantity, setQuantity] = useState(1);
+
+
     return (
-        <div>
-            Product
+        <div className="product">
+            <div className="left">
+                <div className="images">
+                    <img
+                        src={images[0]}
+                        alt=""
+                        onClick={(e) => setSelectedImg(0)}
+                    />
+                    <img
+                        src={images[1]}
+                        alt=""
+                        onClick={(e) => setSelectedImg(1)}
+                    />
+                </div>
+                <div className="mainImg">
+                    <img
+                        src={images[selectedImg]}
+                        alt=""
+                    />
+                </div>
+            </div>
+            <div className="right">
+                <h1>Title</h1>
+                <span className="price">$199</span>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <div className="quantity">
+                    <button
+                        onClick={() =>
+                            setQuantity((prev) => (prev === 1 ? 1 : prev - 1))
+                        }
+                    >
+                        -
+                    </button>
+                    {quantity}
+                    <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+                </div>
+                <button
+                    className="add"
+
+                >
+                    <AddShoppingCartIcon /> ADD TO CART
+                </button>
+                <div className="links">
+                    <div className="item">
+                        <FavoriteBorderIcon /> ADD TO WISH LIST
+                    </div>
+                    <div className="item">
+                        <BalanceIcon /> ADD TO COMPARE
+                    </div>
+                </div>
+                <div className="info">
+                    <span>Vendor: Polo</span>
+                    <span>Product Type: T-Shirt</span>
+                    <span>Tag: T-Shirt, Women, Top</span>
+                </div>
+                <hr />
+                <div className="info">
+                    <span>DESCRIPTION</span>
+                    <hr />
+                    <span>ADDITIONAL INFORMATION</span>
+                    <hr />
+                    <span>FAQ</span>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Product;
