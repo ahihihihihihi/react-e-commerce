63656ef6fbb93ac64b74143c33589e110bdc5821
ahihi <bdsthocu20@gmail.com>
2024-01-03 17:00:45 +0700

    #13

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,71 +1,14 @@
-import React, { useEffect, useState } from "react";
+
+import useFetch from "../../hooks/useFetch";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
-import axios from 'axios'
-
-// const data = [
-//     {
-//         id: 1,
-//         img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
-//         img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-//         title: "Long Sleeve Graphic T-shirt",
-//         isNew: true,
-//         oldPrice: 19,
-//         price: 12,
-//     },
-//     {
-//         id: 2,
-//         img:
-//             "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
-//         img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-//         title: "Coat",
-//         isNew: true,
-//         oldPrice: 19,
-//         price: 12,
-
-//     },
-
-//     {
-//         id: 3,
-//         img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinys rgb&w=1600",
-//         img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-//         title: "Skirt",
-//         oldPrice: 19,
-//         price: 12,
-//     },
-//     {
-//         id: 4,
-//         img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinys rgb&w=1600",
-//         img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-//         title: "Hat",
-//         oldPrice: 19,
-//         price: 12,
-//     },
-// ]
-
 
 
 const FeaturedProducts = ({ type }) => {
 
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await axios.get(import.meta.env.VITE_REACT_APP_API_URL + "/products?populate=*",
-                    {
-                        headers: {
-                            Authorization: "bearer " + import.meta.env.VITE_REACT_APP_API_TOKEN,
-                        }
-                    });
-                console.log(res);
-                setData(res?.data?.data)
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        fetchData();
-    }, []);
+    const { data, loading, error } = useFetch(
+        `/products?populate=*&[filters][type][$eq]=${type}`
+    );
 
     return (
         <div className="featuredProducts">
@@ -80,7 +23,14 @@ const FeaturedProducts = ({ type }) => {
                 </p>
             </div>
             <div className="bottom">
-                {data?.map((item) => <Card item={item} key={item.id} />)}
+                {
+                    error ?
+                        "Something went wrong!"
+                        :
+                        loading ?
+                            "loading"
+                            :
+                            data?.map((item) => <Card item={item} key={item.id} />)}
             </div>
         </div>
     );
--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,53 +1,63 @@
 import React from "react";
 import "./List.scss";
 import Card from "../Card/Card";
+import useFetch from "../../hooks/useFetch";
 
 
-const data = [
-    {
-        id: 1,
-        img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        title: "Long Sleeve Graphic T-shirt",
-        isNew: true,
-        oldPrice: 19,
-        price: 12,
-    },
-    {
-        id: 2,
-        img:
-            "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        title: "Coat",
-        isNew: true,
-        oldPrice: 19,
-        price: 12,
+// const data = [
+//     {
+//         id: 1,
+//         img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+//         img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+//         title: "Long Sleeve Graphic T-shirt",
+//         isNew: true,
+//         oldPrice: 19,
+//         price: 12,
+//     },
+//     {
+//         id: 2,
+//         img:
+//             "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+//         img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+//         title: "Coat",
+//         isNew: true,
+//         oldPrice: 19,
+//         price: 12,
 
-    },
+//     },
 
-    {
-        id: 3,
-        img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinys rgb&w=1600",
-        img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        title: "Skirt",
-        oldPrice: 19,
-        price: 12,
-    },
-    {
-        id: 4,
-        img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinys rgb&w=1600",
-        img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        title: "Hat",
-        oldPrice: 19,
-        price: 12,
-    },
-]
+//     {
+//         id: 3,
+//         img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinys rgb&w=1600",
+//         img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+//         title: "Skirt",
+//         oldPrice: 19,
+//         price: 12,
+//     },
+//     {
+//         id: 4,
+//         img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinys rgb&w=1600",
+//         img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+//         title: "Hat",
+//         oldPrice: 19,
+//         price: 12,
+//     },
+// ]
 
-const List = ({ maxPrice, sort, catId }) => {
+const List = ({ maxPrice, sort, catId, subCats }) => {
+
+    const { data, loading, error } = useFetch(
+        `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
+            (item) => `&[filters][sub_categories][id][$eq]=${item}`
+        )}`
+    );
 
     return (
         <div className="list">
-            {data?.map((item) => <Card item={item} key={item.id} />)}
+            {loading
+                ? "loading"
+                :
+                data?.map((item) => <Card item={item} key={item.id} />)}
         </div>
     )
 };
