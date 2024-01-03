5b63b3bd72cda500198945f89030a3ca56bd28bb
ahihi <bdsthocu20@gmail.com>
2024-01-03 17:13:48 +0700

    #14

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -49,7 +49,7 @@ const List = ({ maxPrice, sort, catId, subCats }) => {
     const { data, loading, error } = useFetch(
         `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
             (item) => `&[filters][sub_categories][id][$eq]=${item}`
-        )}`
+        )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
     );
 
     return (
